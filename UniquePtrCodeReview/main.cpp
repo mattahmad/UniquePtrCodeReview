6845d9b5b22113fb74f07eee63b3f2223ce848fb
matt.a@myplanet.com <matt.a@myplanet.com>
2022-10-10 22:43:40 -0400

    Examination of unique ptr code to support a constructor, destructor,->, * and release()

--- UniquePtrCodeReview/main.cpp
+++ UniquePtrCodeReview/main.cpp
@@ -1,14 +1,40 @@
-//
-//  main.cpp
-//  UniquePtrCodeReview
-//
-//  Created by Matt Ahmad on 2022-10-10.
-//
-
 #include <iostream>
+#include <vector>
+
+template <typename T>
+class Unique_Ptr
+{
+private:
+    T *ptr;
+    
+public:
+    explicit Unique_Ptr(T *p) noexcept : ptr{p} {}
+    ~Unique_Ptr() { delete ptr; };
+    T *operator->() const noexcept { return ptr; }
+    inline T operator*() const { return *ptr; }
+    inline T *get() const noexcept { return ptr; }
+    T *release() noexcept
+    {
+        auto tmp = ptr;
+        ptr = nullptr;
+        return tmp;
+    }
+};
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+    Unique_Ptr p{new int{2}};
+    std::cout << *p << "\n";
+    
+    Unique_Ptr pp{new std::vector<int>{1, 2, 3}};
+    std::cout << pp->size() << "\n";
+    
+    auto test_release = pp.release();
+    std::cout << (pp.get() == nullptr) << "\n";
+    std::cout << test_release->size() << "\n";
+    
     return 0;
 }
+
+
+
+//Implement a simple unique_ptr supporting only a constructor, destructor,->, * and release(). In //particular, don't try to implement an assignment or a copy constructor
